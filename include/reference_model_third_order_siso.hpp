3158e733453ebaa020815479b12e21043d8e7073
Markus Fossdal <51286688+markusfossdal@users.noreply.github.com>
2024-06-14 16:23:29 +0200

    code structure in place

---
+++ include/reference_model_third_order_siso.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+class ReferenceFilterThirdOrderSiso{
+    public:
+        void printolino();
+    private:
+        double test;
+
+};
---
+++ src/reference_model_third_order_siso.cpp
@@ -0,0 +1,6 @@
+#include "reference_model_third_order_siso.hpp"
+
+
+void ReferenceFilterThirdOrderSiso::printolino(){
+    std::cout << "ayo" << std::endl;
+};
